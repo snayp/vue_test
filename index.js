7a75e412e8c8e6221f03b137e0fc890269649820
dsh <snayp@yandex.ru>
2019-01-31 23:20:11 +0300

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+/**
+ * Created by dsh on 31.01.19.
+ */
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Привет, Vue!'
+    }
+})
+
+var app3 = new Vue({
+    el: '#app-3',
+    data: {
+        seen: true
+    }
+})
+var app5 = new Vue({
+    el: '#app-5',
+    data: {
+        message: 'Привет, Vue.js!'
+    },
+    methods: {
+        reverseMessage: function () {
+            this.message = this.message.split('').reverse().join('')
+        }
+    }
+})
