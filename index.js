346fc7b833cc62a1311de682fa867890b54d444f
dsh <dmitry.shilin@datalight.me>
2019-02-04 17:37:12 +0300

    second commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var app = new Vue({
 var app3 = new Vue({
     el: '#app-3',
     data: {
-        seen: true
+        seen: false
     }
 })
 var app5 = new Vue({
@@ -29,5 +29,41 @@ var app6 = new Vue({
     el: '#app-6',
     data: {
         message: 'Привет, Vue!'
+    }
+})
+new Vue({
+    el: '#app-7',
+    data() {
+        return {
+            info: null
+        };
+    },
+    mounted() {
+        axios
+            .get('https://api.coindesk.com/v1/bpi/currentprice.json')
+            .then(response => (this.info = response.data.bpi));
+    },
+    filters: {
+        currencydecimal (value) {
+            return value.toFixed(2)
+        }
+    }
+})
+new Vue({
+    el: '#app-8',
+    data() {
+        return {
+            info: null
+        };
+    },
+    mounted() {
+        axios
+            .get('https://api.datalight.me/v1/request/coinmarketcap.coin.price_usd/?coin=eth,btc,eos&limit=100&offset=0&order=date&to_date=2017-10-20T21:09:53')
+            .then(response => (this.info = response.data.result));
+    },
+    filters: {
+        currencydecimal (value) {
+            return value.toFixed(2)
+        }
     }
 })
